b9fa428216f7b1351fb088edcd1c9ccefd20d711
Eason <30045503+Eason0729@users.noreply.github.com>
2024-05-26 19:48:26 +0800

    init hw5

---
+++ hw5/src/Tree/Bincode.java
@@ -0,0 +1,12 @@
+package Tree;
+
+public interface Bincode {
+    public static byte[] archive() {
+        return null;
+    }
+
+    public void unarchive(byte[] data);
+    public static int get_size() {
+        return 0;
+    }
+}
